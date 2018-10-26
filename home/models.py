a01eb55eaec428c133fe77f06fc7010cb56c3889
Brian Blosser <bpb54321@gmail.com>
2018-10-26 16:20:37 -0400

    Added a Streamfiled model for carousel items

--- home/models.py
+++ home/models.py
@@ -7,11 +7,20 @@ from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.images.blocks import ImageChooserBlock
 
 
+class CarouselItemBlock(blocks.StructBlock):
+    heading = blocks.CharBlock()
+    text = blocks.RichTextBlock()
+    button_text = blocks.CharBlock()
+    button_link = blocks.URLBlock()
+    background_image = ImageChooserBlock()
+
+
 class HomePage(Page):
     body = StreamField([
         ('heading', blocks.CharBlock(classname="full title")),
         ('paragraph', blocks.RichTextBlock()),
         ('image', ImageChooserBlock()),
+        ('carousel_items', blocks.ListBlock(CarouselItemBlock())),
     ])
 
     content_panels = Page.content_panels + [
