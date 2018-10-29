e1f4cd2493391f7d2ba689e8a1553ab796872ae0
Brian Blosser <bpb54321@gmail.com>
2018-10-29 13:44:57 -0400

    Added in responsive image template tag that someone had added to GitHub.

--- home/models.py
+++ home/models.py
@@ -5,6 +5,29 @@ from wagtail.core.fields import StreamField
 from wagtail.core import blocks
 from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.images.blocks import ImageChooserBlock
+from wagtail.images.models import Image, AbstractImage, AbstractRendition
+
+
+class CustomImage(AbstractImage):
+    # Add any extra fields to image here
+
+    caption = models.CharField(max_length=255, blank=True)
+
+    admin_form_fields = Image.admin_form_fields + (
+        # Then add the field names here to make them appear in the form:
+        'caption',
+    )
+
+
+class CustomRendition(AbstractRendition):
+    image = models.ForeignKey(
+        CustomImage, on_delete=models.CASCADE, related_name='renditions'
+    )
+
+    class Meta:
+        unique_together = (
+            ('image', 'filter_spec', 'focal_point_key'),
+        )
 
 
 class CarouselItemBlock(blocks.StructBlock):
@@ -23,11 +46,8 @@ class HomePage(Page):
         ('heading', blocks.CharBlock(classname="full title")),
         ('paragraph', blocks.RichTextBlock()),
         ('image', ImageChooserBlock()),
-        ('carousel_items', blocks.ListBlock(
-                CarouselItemBlock(),
-                template='blocks/carousel_items.html',
-            )
-        ),
+        ('carousel_items', blocks.ListBlock(CarouselItemBlock(),
+            template='blocks/carousel_items.html')),
     ])
 
     content_panels = Page.content_panels + [
