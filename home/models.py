e7e054bac49f2bfb3e08070fffc6660e03bbc7d1
Brian Blosser <bpb54321@gmail.com>
2018-10-29 14:25:28 -0400

    Added a SummaryBlock.

--- home/models.py
+++ home/models.py
@@ -5,29 +5,6 @@ from wagtail.core.fields import StreamField
 from wagtail.core import blocks
 from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.images.blocks import ImageChooserBlock
-from wagtail.images.models import Image, AbstractImage, AbstractRendition
-
-
-class CustomImage(AbstractImage):
-    # Add any extra fields to image here
-
-    caption = models.CharField(max_length=255, blank=True)
-
-    admin_form_fields = Image.admin_form_fields + (
-        # Then add the field names here to make them appear in the form:
-        'caption',
-    )
-
-
-class CustomRendition(AbstractRendition):
-    image = models.ForeignKey(
-        CustomImage, on_delete=models.CASCADE, related_name='renditions'
-    )
-
-    class Meta:
-        unique_together = (
-            ('image', 'filter_spec', 'focal_point_key'),
-        )
 
 
 class CarouselItemBlock(blocks.StructBlock):
@@ -41,6 +18,17 @@ class CarouselItemBlock(blocks.StructBlock):
         template = 'blocks/carousel_item_block.html'
 
 
+class SummaryBlock(blocks.StructBlock):
+    heading = blocks.CharBlock()
+    text = blocks.RichTextBlock()
+    button_text = blocks.CharBlock()
+    button_link = blocks.URLBlock()
+    thumbnail_image = ImageChooserBlock()
+
+    class Meta:
+        template = 'blocks/summary_block.html'
+
+
 class HomePage(Page):
     body = StreamField([
         ('heading', blocks.CharBlock(classname="full title")),
@@ -48,6 +36,9 @@ class HomePage(Page):
         ('image', ImageChooserBlock()),
         ('carousel_items', blocks.ListBlock(CarouselItemBlock(),
             template='blocks/carousel_items.html')),
+        ('summary_blocks', blocks.ListBlock(SummaryBlock(),
+            template='blocks/summary_blocks.html')),
+
     ])
 
     content_panels = Page.content_panels + [
